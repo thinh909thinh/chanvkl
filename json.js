e90e70a3c200c82681f3cacdeda11cfc5ce0536e
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-05-03 17:35:50 +0700

    Add files via upload

---
+++ json.js
@@ -0,0 +1,5 @@
+// 1: json là 1 đọng dạng dữ liệu 'chuỗi'  ( k phải là 1 kiểu dữ liệu)
+// 2: JAVASCRIP Object NOTATION
+//3: JSON giúp b thể hiện các dạng dữ liệu:string , number , boolean,Null, Array,Object.
+// Stringify : từ javascrip --> JSON
+// prasse:từ JSON-----> javascrip 
